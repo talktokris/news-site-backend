d31bfe3822b88b9772a7760363cee2f832462373
talktokris <talktokris@gmail.com>
2023-06-05 14:05:01 +0545

    user setings save  delete api improved

--- app/Http/Controllers/Api/ProfileController.php
+++ app/Http/Controllers/Api/ProfileController.php
@@ -40,8 +40,6 @@ class ProfileController extends Controller
 
     public function saveUserSetting(Request $request){
 
-
-
         $id = auth('sanctum')->user()->id;
        // return $id;
         $type= $request['settingName'];
@@ -49,91 +47,86 @@ class ProfileController extends Controller
 
         if($type==='source'){
 
-              $newSourceDataCount = Users_source_setting::where('id', $fill_id)->get()->count(); 
+       
               $newSourceData = News_source::where('id', $fill_id)->get()->toArray(); 
-            if($newSourceDataCount>=1){
-              return $this->jsonReturn('error', 'This setting is already exist');
-            } else {
               if(count($newSourceData)>=1){
-                $SaveData = new Users_source_setting;
-                $SaveData->user_id=$id;
-                $SaveData->name = $newSourceData[0]['name'];
-                $SaveData->label =$newSourceData[0]['label'];
-                $SaveData->status = 1;
-                $SaveData->save();
-                return $this->jsonReturn('success', 'Setting saved successfully');
-              } 
-
-           }
-          } elseif($type==='category'){
-
-          $newSourceDataCount = Users_category_setting::where('id', $fill_id)->get()->count(); 
-          $newSourceData = News_category::where('id', $fill_id)->get()->toArray(); 
-            if($newSourceDataCount>=1){
-              return $this->jsonReturn('error', 'This setting is already exist');
-            } else {
-              if(count($newSourceData)>=1){
-                $SaveData = new Users_category_setting;
-                $SaveData->user_id=$id;
-                $SaveData->name = $newSourceData[0]['name'];
-                $SaveData->label =$newSourceData[0]['label'];
-                $SaveData->status = 1;
-                $SaveData->save();
-                return $this->jsonReturn('success', 'Setting saved successfully');
+                $newSourceDataCount = Users_source_setting::where('user_id', $id)->where('name', $newSourceData[0]['name'])->get()->count(); 
+                
+                if($newSourceDataCount>=1){
+                  return $this->jsonReturn('error', 'This source setting is already exist');
+                } else {
+               // return $newSourceData[0]['name'];
+                  $SaveData = new Users_source_setting;
+                  $SaveData->user_id=$id;
+                  $SaveData->name = $newSourceData[0]['name'];
+                  $SaveData->label =$newSourceData[0]['label'];
+                  $SaveData->status = 1;
+                  $SaveData->save();
+                    if($SaveData){
+                      return $this->jsonReturn('success', 'Source setting saved successfully');
+                    } else { return $this->jsonReturn('error', 'Known error');}
+                    
+                  }
 
               }
-          
+          } elseif($type==='category'){
+
+          $newSourceData = News_category::where('id', $fill_id)->get()->toArray(); 
+     
+              if(count($newSourceData)>=1){
+
+                $newSourceDataCount = Users_category_setting::where('user_id', $id)->where('name',$newSourceData[0]['name'])->get()->count(); 
+                  if($newSourceDataCount>=1){
+                    return $this->jsonReturn('error', 'This category setting is already exist');
+                  } else {
+                    $SaveData = new Users_category_setting;
+                    $SaveData->user_id=$id;
+                    $SaveData->name = $newSourceData[0]['name'];
+                    $SaveData->label =$newSourceData[0]['label'];
+                    $SaveData->status = 1;
+                    $SaveData->save();
+                    if($SaveData){
+                      return $this->jsonReturn('success', 'Category setting saved successfully');
+                    } else { return $this->jsonReturn('error', 'Known error');}
+                 
+                  }
+
             }
+          
         } 
         elseif($type==='author'){
 
-          $newSourceDataCount = Users_authors_setting::where('id', $fill_id)->get()->count(); 
           $newSourceData = Author_list::where('id', $fill_id)->get()->toArray(); 
-            if($newSourceDataCount>=1){
-              return $this->jsonReturn('error', 'This setting is already exist');
-            } else {
+    
               if(count($newSourceData)>=1){
-                $SaveData = new Users_authors_setting;
-                $SaveData->user_id=$id;
-                $SaveData->name = $newSourceData[0]['name'];
-                $SaveData->label =$newSourceData[0]['label'];
-                $SaveData->status = 1;
-                $SaveData->save();
-                return $this->jsonReturn('success', 'Setting saved successfully');
+
+                $newSourceDataCount = Users_authors_setting::where('user_id', $id)->where('name', $newSourceData[0]['name'])->get()->count(); 
+                  if($newSourceDataCount>=1){
+                    return $this->jsonReturn('error', 'This auther setting is already exist');
+                  } else {
+                    $SaveData = new Users_authors_setting;
+                    $SaveData->user_id=$id;
+                    $SaveData->name = $newSourceData[0]['name'];
+                    $SaveData->label =$newSourceData[0]['label'];
+                    $SaveData->status = 1;
+                    $SaveData->save();
+                    if($SaveData){
+                      return $this->jsonReturn('success', 'Auther setting saved successfully');
+                    } else { return $this->jsonReturn('error', 'Known error');}
+                   
+                  }
 
               }
           
-            }
 
         }
 
   
-        // dd($newSourceData);
        
-        
-
-        $souresData = Users_source_setting::where('user_id',$id)->get();
-        $categoriesData = Users_category_setting::where('user_id',$id)->get();
-        $authoresData = Users_authors_setting::where('user_id',$id)->get();
-
-
-
-     //  return $user_info = User::where('id',$id)->get();
-
-          return response()->json([
-              'status' => 'success',
-              'user_sources' => UserSourceResource::collection($souresData),
-              'user_categories' =>  UserCategoryResource::collection($categoriesData),
-              'user_authors' =>  UserAuthorResource::collection($authoresData),
-             // 'results' => UserProfileResource::collection($user_info);
-            
-          ]);
     }
 
 
     public function deleteUserSetting(Request $request){
-
-
 
       $id = auth('sanctum')->user()->id;
      // return $id;
@@ -142,40 +135,26 @@ class ProfileController extends Controller
 
       if($type==='source'){
           $SaveData = Users_source_setting::where('id','=', $fill_id)->where('user_id','=', $id)->delete();;
-          return $this->jsonReturn('success', 'Setting deleted successfully');
+        if($SaveData){
+           return $this->jsonReturn('success', 'Source setting deleted successfully');
+          }else { return $this->jsonReturn('error', 'Known error');}
          
       } elseif($type==='category'){
           $SaveData = Users_category_setting::where('id','=', $fill_id)->where('user_id','=', $id)->delete();;
-          return $this->jsonReturn('success', 'Setting deleted successfully');
+          if($SaveData){
+          return $this->jsonReturn('success', 'Category setting deleted successfully');
+          }else { return $this->jsonReturn('error', 'Known error');}
       } 
       elseif($type==='author'){
-
-        $SaveData = Users_authors_setting::where('id','=', $fill_id)->where('user_id','=', $id)->delete();;
-        return $this->jsonReturn('success', 'Setting deleted successfully');
+       
+          $SaveData = Users_authors_setting::where('id','=', $fill_id)->where('user_id','=', $id)->delete();;
+          if($SaveData){
+             return $this->jsonReturn('success', 'Author setting deleted successfully');
+        } else { return $this->jsonReturn('error', 'Known error');}
 
       }
 
 
-      // dd($newSourceData);
-     
-      
-
-      $souresData = Users_source_setting::where('user_id',$id)->get();
-      $categoriesData = Users_category_setting::where('user_id',$id)->get();
-      $authoresData = Users_authors_setting::where('user_id',$id)->get();
-
-
-
-   //  return $user_info = User::where('id',$id)->get();
-
-        return response()->json([
-            'status' => 'success',
-            'user_sources' => UserSourceResource::collection($souresData),
-            'user_categories' =>  UserCategoryResource::collection($categoriesData),
-            'user_authors' =>  UserAuthorResource::collection($authoresData),
-           // 'results' => UserProfileResource::collection($user_info);
-          
-        ]);
   }
 
     public function jsonReturn($status, $message){
